673d66daec4ac8b723eda33f9e01631b2e908baf
BdourAlzeer <bedourabed@hotmail.com>
2021-04-01 09:06:43 +0300

    Update FirstClass.java

--- TestProjectAdv/src/FirstClass.java
+++ TestProjectAdv/src/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
 	public static void main(String[] args) {
 		//added a new print statement.
 		System.out.println("First Pull");
+		
+		System.out.println("Second pull comment");
+
 
 	}
 
